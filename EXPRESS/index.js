d22d15eed829fc98b4ea307f651d4706b71ab0a6
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-04 00:54:39 -0300

    criação da primeira rota utilizando express

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -1,6 +1,19 @@
 const express = require("express"); //Importa
 const app = express(); //Iniciando o express em app
 
+
+app.get("/", function (req, res) {
+    res.send("bem vindo!")
+});
+
+app.get("/blog", function (req, res) {
+    res.send("bem vindo ao blog!")
+});
+
+app.get("/canal", function (req, res) {
+    res.send("<h1>bem vindo ao canal!</h1>")
+});
+
 app.listen(8000, function (erro) {
     if (erro) {
         console.log("ocorreu um erro!");
