9aad417760850c461073e0288589385449866b85
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-04 17:33:17 -0300

    trabalhando com variaveis no ejs

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("home/home");
+app.get("/:nome/:lang", (req, res) => {
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    res.render("index", {
+        nome: nome,
+        lang: lang,
+        empresa: "Guia do programador",
+        inscritos: 8000
+    });
 });
 
 app.listen(8000, () => {
