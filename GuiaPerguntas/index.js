e7824509b9372bbc9d1524c565f33a0bc8ff1d55
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-04 18:19:10 -0300

    estudo de estruturas de repetição com o ejs

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -7,12 +7,23 @@ app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
     var exibirMsg = false;
+
+    var produtos = [
+        { nome: "doritos", preco: 3.14 },
+        { nome: "coca-cola", preco: 5 },
+        { nome: "frango", preco: 10 },
+        { nome: "maça", preco: 2 },
+        { nome: "leite", preco: 1.45 }
+    ]
+
+
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
         inscritos: 8000,
-        msg: exibirMsg
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
