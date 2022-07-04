943e3c484b60e324c49d8f5d586ce4f91164a8a0
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-04 17:54:28 -0300

    estudo de condicionais com ejs

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs');
 app.get("/:nome/:lang", (req, res) => {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = false;
     res.render("index", {
         nome: nome,
         lang: lang,
         empresa: "Guia do programador",
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg
     });
 });
 
