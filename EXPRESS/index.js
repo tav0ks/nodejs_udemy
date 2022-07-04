040d5d221c6521425346b32bb15e0d8756ff3c1e
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-04 01:14:11 -0300

    trabalhando com rotas utilizando parametros obrigatorios e opcionais

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -6,12 +6,31 @@ app.get("/", function (req, res) {
     res.send("bem vindo!")
 });
 
-app.get("/blog", function (req, res) {
+
+//PARAMTROS OPCIONAIS
+app.get("/blog/:artigo?", function (req, res) {
+
+    var artigo = req.params.artigo;
+
+    if (artigo) {
+        res.send("<h2>Artigo: " + artigo + "</h2>")
+    } else {
+        res.send("<h3>Bem vindo ao blog!</h3>")
+    }
+
     res.send("bem vindo ao blog!")
 });
 
 app.get("/canal", function (req, res) {
     res.send("<h1>bem vindo ao canal!</h1>")
+});
+
+
+//PARAMETROS OBRIGATORIOS
+app.get("/ola/:nome/:empresa", function (req, res) {
+    var nome = req.params.nome;
+    var empresa = req.params.empresa;
+    res.send("<h1> Olá " + nome + " da empresa " + empresa + "</h1>");
 });
 
 app.listen(8000, function (erro) {
