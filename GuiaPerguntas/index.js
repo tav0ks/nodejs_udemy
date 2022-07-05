8269bb87b31e61be72797c5e2aad90be30d01a5c
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-05 19:30:45 -0300

    Realização da conexão com o banco de dados utilizando o sequelize

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -1,11 +1,23 @@
-// express
 const express = require("express"); // importanto o express
 const app = express();  // passando o express para a const app
+const bodyParser = require("body-parser"); // importando o body parser
+const connection = require("./database/database");
+
+//database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com o banco de dados!")
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    });
+
+// express
 app.set('view engine', 'ejs'); // definindo o ejs como view engine do express
 app.use(express.static('public')); // define a pasta de arquivos estaticos do express
 
 //body parser
-const bodyParser = require("body-parser"); // importando o body parser
 app.use(bodyParser.urlencoded({ extended: false })); // transforma dados do formulario HTML em arquivo javascript
 app.use(bodyParser.json()); // transforma dados do formulario json para javascript
 
