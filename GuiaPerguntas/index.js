13baf5d39ae72c1c6f41ad7bcdfc22b78bb4a5cd
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-05 13:57:39 -0300

    Instalação do bootstrap

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -5,28 +5,8 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 
-app.get("/:nome/:lang", (req, res) => {
-    var nome = req.params.nome;
-    var lang = req.params.lang;
-    var exibirMsg = false;
-
-    var produtos = [
-        { nome: "doritos", preco: 3.14 },
-        { nome: "coca-cola", preco: 5 },
-        { nome: "frango", preco: 10 },
-        { nome: "maça", preco: 2 },
-        { nome: "leite", preco: 1.45 }
-    ]
-
-
-    res.render("index", {
-        nome: nome,
-        lang: lang,
-        empresa: "Guia do programador",
-        inscritos: 8000,
-        msg: exibirMsg,
-        produtos: produtos
-    });
+app.get("/", (req, res) => {
+    res.render("index");
 });
 
 app.listen(8000, () => {
