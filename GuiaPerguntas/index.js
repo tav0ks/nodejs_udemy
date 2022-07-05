d10c3b08b8a9e2de34f08cb53abe103ad84e2e89
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-05 15:48:24 -0300

    instalação e configuração do body parser e adequação da rota para receber os dados do formulario

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -1,10 +1,15 @@
-const express = require("express");
-const app = express();
+// express
+const express = require("express"); // importanto o express
+const app = express();  // passando o express para a const app
+app.set('view engine', 'ejs'); // definindo o ejs como view engine do express
+app.use(express.static('public')); // define a pasta de arquivos estaticos do express
 
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
+//body parser
+const bodyParser = require("body-parser"); // importando o body parser
+app.use(bodyParser.urlencoded({ extended: false })); // transforma dados do formulario HTML em arquivo javascript
+app.use(bodyParser.json()); // transforma dados do formulario json para javascript
 
-
+//rotas
 app.get("/", (req, res) => {
     res.render("index");
 });
@@ -14,7 +19,9 @@ app.get("/perguntar", (req, res) => {
 });
 
 app.post("/salvarpergunta", (req, res) => {
-    res.send("Formulario Recebido!");
+    var titulo = req.body.titulo;
+    var descricao = req.body.descricao;
+    res.send("Formulario Recebido! | titulo: " + titulo + " | descrição: " + descricao);
 });
 
 app.listen(8000, () => {
