bc468bf9bc402375840cdbdcf1924f1f1e5b59f1
Vinícius Tavolaro <viniciustavolaro@gmail.com>
2022-07-06 17:24:15 -0300

    tratamento do front-end de exibição das respostas

--- GuiaPerguntas/index.js
+++ GuiaPerguntas/index.js
@@ -61,12 +61,30 @@ app.get("/pergunta/:id", (req, res) => {
         where: { id: id }
     }).then(pergunta => {
         if (pergunta != undefined) {
-            res.render("pergunta", {
-                pergunta: pergunta
+
+            Resposta.findAll({
+                where: { perguntaId: pergunta.id },
+                order: [['id', 'DESC']]
+            }).then(respostas => {
+                res.render("pergunta", {
+                    pergunta: pergunta,
+                    respostas: respostas
+                });
             });
         } else {
             res.redirect("/");
         }
+    });
+});
+
+app.post("/responder", (req, res) => {
+    var corpo = req.body.corpo;
+    var perguntaId = req.body.pergunta;
+    Resposta.create({
+        corpo: corpo,
+        perguntaid: perguntaId
+    }).then(() => {
+        res.redirect("/pergunta/" + perguntaId);
     });
 });
 
